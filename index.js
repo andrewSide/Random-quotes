04770f0c035ffeeb88d238804534551cc0548b06
AndrewSide <agorban2396@gmail.com>
2025-03-31 14:12:55 +0300

    Show favorite quotes below current quote

--- index.js
+++ index.js
@@ -3,15 +3,50 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const quoteAuthorElement = document.getElementById("quore-author");
 const generateBtn = document.getElementById("generate-btn");
+const toggleFavoriteBtn = document.getElementById("toggle-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+
+let currentQuoteIndex;
 
 function generateRandomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
+  currentQuoteIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[currentQuoteIndex];
   const { quote, author: quoteAuthor } = randomQuote;
   quoteElement.textContent = quote;
   quoteAuthorElement.textContent = quoteAuthor;
+  toggleFavoriteBtn.textContent = randomQuote.isFavorite
+    ? "Видалити цитату"
+    : "Зберегти";
+
+  toggleFavoriteBtn.style.display = "inline-block";
+}
+
+function toggleFavorite() {
+  const currentQuote = quotes[currentQuoteIndex];
+  currentQuote.isFavorite = !currentQuote.isFavorite;
+  toggleFavoriteBtn.textContent = currentQuote.isFavorite
+    ? "Видалити цитату"
+    : "Зберегти";
+
+  if (currentQuote.isFavorite) {
+    const favoriteCard = document.createElement("div");
+    favoriteCard.classList.add("favorite-card");
+    favoriteCard.innerHTML = `
+      <p>${currentQuote.quote}</p>
+      <p class="author">${currentQuote.author}</p>
+      `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll(".favorite-card");
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(currentQuote.quote)) {
+        card.remove();
+      }
+    });
+  }
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
+toggleFavoriteBtn.addEventListener("click", toggleFavorite);
 
 // generateRandomQuote();
